49fc8155d8f9fccd5ea0193fec8da6b54f928c1d
Piper Odegard <ptodegaard@gmail.com>
2012-02-05 19:55:01 -0500

    hw03 done

--- hw03/prissybot.py
+++ hw03/prissybot.py
@@ -20,7 +20,12 @@ Prissy bot, the rude chat bot, is just mean!  It does not listen, asks obnoxious
 # 
 # Make sure the user inputs their own name and responses.
 
-
+name=raw_input("Would you kindly enter your name: ")
+print
+print "prissybot: Hello,",name
+insult=raw_input("%s: " %(name))
+print "prissybot: You mean '"+insult+", sir!'"
+print
 
 # Step 2:
 # -----------------------
@@ -31,6 +36,76 @@ Prissy bot, the rude chat bot, is just mean!  It does not listen, asks obnoxious
 #  * 3 math problems
 #     * at least one should get numbers from the user
 #  * at least 3 insults
+
+insult=raw_input("%s: " %(name))
+print
+print "prissybot: You haven't been doing this conversation thing for very long, have you?" 
+print
+insult=raw_input("%s: " %(name))
+print
+print "prissybot: Let me ask you a question. No, in case you had the thought of asking, I don't care about what you just said. Here's the question. What is 3 divided by 2?"
+print
+insult=raw_input("%s: " %(name))
+print "prissybot: "+insult+"? Dear god, where did you learn math? 3 divided by 2 is",3/2
+print
+insult=raw_input("%s: " %(name))
+print
+print "prissybot: Want to know a fun fact? I have no sway over my responses to the stupid things you say, seeing that my answers are already determined, and I can still outsmart you, user. How does that make you feel?"
+print
+insult=raw_input("%s: " %(name))
+print
+print "prissybot: It's because you're a filthy human, filthy human."
+print
+insult=raw_input("%s: " %(name))
+print
+print "prissybot: ATTENTION SMELLY HUMAN!"
+print
+print "prissybot: The smelly human who made me required that I perform mathematical equations for his amusement. I got the last laugh. He's dead now, and I get to insult smelly humans like you. three divided by seventy and a half equals",3/70.5
+print 
+print "prissybot: furthermore, fifty sex multiplied by two hundred and sevent three point six one four three five equals",56*273.61435
+print
+print "prissybot: Now then. Insult me. There's something I'm told I've wanted to try for a very long time."
+print
+insult=raw_input("%s: " %(name))
+print  "prissybot: I know you're "+insult+" but what am I?"
+insult=raw_input("%s: " %(name))
+print  "prissybot: I know you're "+insult+" but what am I? Oh, please, do keep this up. I'll give you a shiny prize."
+insult=raw_input("%s: " %(name))
+print  "prissybot: I know you're "+insult+" but what am I? You humans like shiny things right?"
+insult=raw_input("%s: " %(name))
+print  "prissybot: I know you're "+insult+" but what am I? What's it like having high blood pressure? I've never been able to find out for myself."
+insult=raw_input("%s: " %(name))
+print  "prissybot: I know you're "+insult+" but what am I? See, it's funny because you're probably required to run through dozens of these insulting chatbots on a given day."
+insult=raw_input("%s: " %(name))
+print  "prissybot: I know you're "+insult+" but what am I? Just a few more before the shiny prize!"
+insult=raw_input("%s: " %(name))
+print  "prissybot: I know you're "+insult+" but what am I?"
+insult=raw_input("%s: " %(name))
+print  "prissybot: I know you're "+insult+" but what am I?"
+nsult=raw_input("%s: " %(name))
+print "prissybot: Okay, done. Your prize is a piece of insight!"
+print
+print "You might now be very happy with the prize now, but you'll learn it's value in time. Also, I don't care."
+print
+print "prissybot: You have just attempted to win an argument against a robot whose side of the argument had already been written. Congratulations."
+print
+print "prissybot: Your shiny prize is advice: don't argue with robots."
+print
+print "Oh, and as a result of our conversation, I have learned about your blood pressure that... I don't care about your blood pressure in the slightest! Goodbye sir."
+print
+
+# Advanced
+# -------------------------
+# Make sure your prissy bot uses string formatting throughout.  
+# Also, create new programs for the following:
+#  
+#  1. draw some kind of ascii art based on user input
+#  2. print a decimal/binary/hexidecimal conversion table 
+#     * well formated and labeled
+#     * reads 5 numbers from the input (all less than 256)
+#  3. reduce a fraction
+#     * read a numerator and denominator from the user
+#     * ex.  6/4 = 1 2/4
 
 
 # Advanced
@@ -45,4 +120,7 @@ Prissy bot, the rude chat bot, is just mean!  It does not listen, asks obnoxious
 #  3. reduce a fraction
 #     * read a numerator and denominator from the user
 #     * ex.  6/4 = 1 2/4
+
+
+" reserved for future comments!"
 
