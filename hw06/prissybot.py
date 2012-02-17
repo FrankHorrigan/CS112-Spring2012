65a5bc781ae60a24641db3cfcab16e16e078a488
Piper Odegard <ptodegaard@gmail.com>
2012-02-16 21:16:26 -0500

    hw06 done

---
+++ hw06/prissybot.py
@@ -0,0 +1,90 @@
+#!/usr/bin/env python
+
+#Section 1. Asks for your name, by which it will address you in the future. 
+#Begins to be prissy.
+
+#Variables:
+#name = the name that prissybot addresses you with. You enter that at the start.
+#insult = all the following input, a lot of which prissybot regurgitates back at you insultingly.
+
+name=raw_input("Would you kindly enter your name: ")
+print
+print "prissybot: Hello,",name
+insult=raw_input("%s: " %(name))
+print "prissybot: You mean '"+insult+", sir!'"
+print
+
+#Conversation starts.
+
+insult=raw_input("%s: " %(name))
+print
+print "prissybot: You haven't been doing this conversation thing for very long, have you?" 
+print
+insult=raw_input("%s: " %(name))
+print
+
+#This continues for a while - prissybot will ask  you questions, then let you enter whatever you wish, and then insult you.
+
+print "prissybot: Let me ask you a question. No, in case you had the thought of asking, I don't care about what you just said. Here's the question. What is 3 divided by 2?"
+print
+insult=raw_input("%s: " %(name))
+print "prissybot: "+insult+"? Dear god, where did you learn math? 3 divided by 2 is",3/2
+print
+
+#That was the first math problem that prissybot performs.
+
+insult=raw_input("%s: " %(name))
+print
+print "prissybot: Want to know a fun fact? I have no sway over my responses to the stupid things you say, seeing that my answers are already determined, and I can still outsmart you, user. How does that make you feel?"
+print
+insult=raw_input("%s: " %(name))
+print
+print "prissybot: It's because you're a filthy human, filthy human."
+print
+insult=raw_input("%s: " %(name))
+print
+print "prissybot: ATTENTION SMELLY HUMAN!"
+print
+print "prissybot: The smelly human who made me required that I perform mathematical equations for his amusement. I got the last laugh. He's dead now, and I get to insult smelly humans like you. three divided by seventy and a half equals",3/70.5
+print 
+print "prissybot: furthermore, fifty sex multiplied by two hundred and sevent three point six one four three five equals",56*273.61435
+print
+
+#Those were the last two math problems I needed to include.
+#There's a rather long process now of prissybot repeating back "I know you're [input] but what am I?, followed by insults.
+
+print "prissybot: Now then. Insult me. There's something I'm told I've wanted to try for a very long time."
+print
+insult=raw_input("%s: " %(name))
+print  "prissybot: I know you're "+insult+" but what am I?"
+insult=raw_input("%s: " %(name))
+print  "prissybot: I know you're "+insult+" but what am I? Oh, please, do keep this up. I'll give you a shiny prize."
+insult=raw_input("%s: " %(name))
+print  "prissybot: I know you're "+insult+" but what am I? You humans like shiny things right?"
+insult=raw_input("%s: " %(name))
+print  "prissybot: I know you're "+insult+" but what am I? What's it like having high blood pressure? I've never been able to find out for myself."
+insult=raw_input("%s: " %(name))
+print  "prissybot: I know you're "+insult+" but what am I? See, it's funny because you're probably required to run through dozens of these insulting chatbots on a given day."
+
+#Was it fun to run through a bunch of insulting chatbots or was it irritating?
+
+insult=raw_input("%s: " %(name))
+print  "prissybot: I know you're "+insult+" but what am I? Just a few more before the shiny prize!"
+insult=raw_input("%s: " %(name))
+print  "prissybot: I know you're "+insult+" but what am I?"
+insult=raw_input("%s: " %(name))
+print  "prissybot: I know you're "+insult+" but what am I?"
+nsult=raw_input("%s: " %(name))
+
+#All of the following is just an enormous end statement. Enjoy!
+
+print "prissybot: Okay, done. Your prize is a piece of insight!"
+print
+print "You might now be very happy with the prize now, but you'll learn it's value in time. Also, I don't care."
+print
+print "prissybot: You have just attempted to win an argument against a robot whose side of the argument had already been written. Congratulations."
+print
+print "prissybot: Your shiny prize is advice: don't argue with robots."
+print
+print "Oh, and as a result of our conversation, I have learned about your blood pressure that... I don't care about your blood pressure in the slightest! Goodbye sir."
+print
